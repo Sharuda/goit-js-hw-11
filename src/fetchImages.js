47ff24f8e7285626751b57ac4c214f26fe563997
Sharuda <sharuda.yana98@gmail.com>
2023-03-29 12:24:26 +0300

    add

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -3,10 +3,18 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '34840339-667e92b06a5cc18aaab785dc8';
 
-let page = 1;
+export class PixabayApi {
+  page = 1;
+  query = null;
 
-export function FetchImages(query) {
-  return axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&${page}`
-  );
+  fetchImages() {
+    return axios.get(`${BASE_URL}?key=${API_KEY}`, {
+      params: {
+        query: this.query,
+        page: this.page,
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,60 @@
 import './css/styles.css';
-import { FetchImages } from './fetchImages';
+import { PixabayApi } from './fetchImages';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl = document.querySelector('#search-form');
+const containerEl = document.querySelector('.gallery');
 
+const pixabayApi = new PixabayApi();
 
+formEl.addEventListener('submit', handleSearchImages);
+
+function handleSearchImages(event) {
+  event.preventDefault();
+
+  const searchQuery = event.currentTarget.value;
+  pixabayApi.query = searchQuery;
+  pixabayApi.page = 1;
+
+  pixabayApi
+    .fetchImages()
+    .then(({ data }) => {
+      if (!data.results.length) {
+        Notify.failure(
+          '"Sorry, there are no images matching your search query. Please try again."'
+        );
+        return;
+      }
+
+      containerEl.innerHTML = createGalleryImages(data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function createGalleryImages(array) {
+  const markup = array
+    .map(({ likes, views, comments, downloads, tags, webformatURL }) => {
+      return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+
+  containerEl.insertAdjacentHTML('beforeend', markup);
+}
